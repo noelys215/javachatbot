8a6adbda747198fba2716dffe598111ac94f0219
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-03-17 13:59:11 -0400

    step four

--- Simple Chat Bot with Java/task/src/bot/SimpleBot.java
+++ Simple Chat Bot with Java/task/src/bot/SimpleBot.java
@@ -5,31 +5,28 @@ import java.util.Scanner;
 public class SimpleBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Introducing the bot
+
         System.out.println("Hello! My name is Aid.");
         System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
 
-        // User inputs their name
         String name = scanner.nextLine();
-        System.out.println("What a great name you have, " + name + "!");
 
-        // Guessing the age
+        System.out.println("What a great name you have, " + name + "!");
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
 
-        // reading all remainders
-        // Read the remainders
-        int remainder3 = scanner.nextInt();
-        int remainder5 = scanner.nextInt();
-        int remainder7 = scanner.nextInt();
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
 
-        // Calculate the age based on the remainders and the formula provided
-        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
 
-        // Display the guessed age
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
 
-        scanner.close();
+        // read a number and count to it here
+
+        System.out.println("Completed, have a nice day!");
     }
 }
